8abcbcb34566a2b2efad210f91d421027a48bd80
MMR\T.Karalis <t.karalis@mmr-research.com>
2018-01-03 14:34:56 +0000

    Created View/ViewModel/Model file structure

---
+++ MoodMovies/AppBootStrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MoodMovies.ViewModels;
+using MoodMovies.Views;
+using Caliburn.Micro;
+
+namespace MoodMovies
+{
+    class AppBootStrapper: BootstrapperBase
+    {
+        public AppBootStrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+
+    }
+}

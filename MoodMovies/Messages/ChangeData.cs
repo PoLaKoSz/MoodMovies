1e6a485af66784afde61f43ae75aba863049e670
MMR\T.Karalis <t.karalis@mmr-research.com>
2018-01-05 09:40:29 +0000

    added changedata message class

---
+++ MoodMovies/Messages/ChangeData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMovies.Messages
+{
+    class ChangeData
+    {
+        public String Data
+        {
+            get;
+            private set;
+        }
+
+        public ChangeData(string text)
+        {
+            Data = text;
+        }
+
+    }
+}

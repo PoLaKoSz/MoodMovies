b213cfec902c33069272901afa01a15f7c242325
Tony <karalistony@hotmail.com>
2018-02-01 23:46:26 +0000

    adjusted top drawer height. added listbox with bound to actor collection

---
+++ MoodMovies/Models/Actors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMovies.Models
+{
+    public class Actor
+    {
+        public Actor() { }
+        public Actor( string name, bool ischecked)
+        {
+            ActorName = name;
+            ActorChecked = ischecked;
+        }
+        public string ActorName { get; set; }
+        public bool ActorChecked { get; set; }
+    }
+}
--- MoodMovies/ViewModels/ASearchViewModel.cs
+++ MoodMovies/ViewModels/ASearchViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using MoodMovies.Models;
 
 namespace MoodMovies.ViewModels
 {
@@ -12,7 +14,25 @@ namespace MoodMovies.ViewModels
         // constructor
         public ASearchViewModel()
         {
+            TestMethod();
+        }
 
+        #region Actor Properties
+        private ObservableCollection<Actor> _actorsCollection;
+        public ObservableCollection<Actor> ActorsCollection { get => _actorsCollection; set { _actorsCollection = value; NotifyOfPropertyChange(); } }
+        #endregion
+
+        private void TestMethod()
+        {
+            _actorsCollection = new ObservableCollection<Actor>();
+            ActorsCollection.Add(new Actor("Dwayne Johnson",false));
+            ActorsCollection.Add(new Actor("Kate Moss", false));
+            ActorsCollection.Add(new Actor("Sylvester Stalone", false));
+            ActorsCollection.Add(new Actor("Arnold Schwatzenegger", false));
+            ActorsCollection.Add(new Actor("John Kuzak", false));
+            ActorsCollection.Add(new Actor("Denzel Washington", false));
         }
     }
+
+    
 }

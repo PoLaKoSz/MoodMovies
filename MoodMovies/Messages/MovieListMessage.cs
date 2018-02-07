15639ab19654f12529e093be4c4b0c51096adec7
Tony <karalistony@hotmail.com>
2018-02-07 22:38:12 +0000

    created movielistmessage that is sent when certain event is fired

---
+++ MoodMovies/Messages/MovieListMessage.cs
@@ -0,0 +1,19 @@
+﻿using MoodMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMovies.Messages
+{
+    public class MovieListMessage
+    {
+        public List<MovieSearchResult> Movielist { get; private set; }
+
+        public MovieListMessage(List<MovieSearchResult> results)
+        {
+            Movielist = results;
+        }
+    }
+}

bf603cea5fc78a63bd8b5493a540b4d146f07cde
Tony <karalistony@hotmail.com>
2018-02-12 22:56:00 +0000

    search history displays dummy data

--- MoodMovies/ViewModels/HistoryViewModel.cs
+++ MoodMovies/ViewModels/HistoryViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using MoodMovies.Models;
 
 namespace MoodMovies.ViewModels
 {
@@ -14,19 +15,26 @@ namespace MoodMovies.ViewModels
         public HistoryViewModel()
         {
             Name = "History";
+            TestMethod();
         }
 
         #region General Properties
         private string _name;
-        public string Name { get => _name; set { _name = value; NotifyOfPropertyChange(() => Name); } }
-        private ObservableCollection<SearchItem> _searchs;
+        public string Name { get => _name; set { _name = value; NotifyOfPropertyChange(() => Name); } }       
+        private ObservableCollection<SearchItem> _searchItems;
+        public ObservableCollection<SearchItem> SearchItems { get => _searchItems; set { _searchItems = value; NotifyOfPropertyChange(); } }
 
         #endregion
 
         #region Methods
         public void TestMethod()
         {
+            SearchItems = new ObservableCollection<SearchItem>();
 
+            for( int i = 0; i< 10; i++)
+            {
+                SearchItems.Add(new SearchItem($"0{i}/02/2018", "The rock movies"));
+            }
         }
         #endregion
     }

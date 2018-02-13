4dd08504c5e767974fd8e81f8ab6ca7787a6caa4
Tony <karalistony@hotmail.com>
2018-02-13 18:58:52 +0000

    moviesearchresult is now a seperate viewmodel with a view

---
+++ MoodMovies/ViewModels/MovieSearchResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMovies.ViewModels
+{
+    public class MovieSearchResultViewModel: Screen
+    {
+
+
+    }
+}

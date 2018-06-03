bc2ebdcb4e671904507fb2474374114a05e3ce44
Tony Karalis <karalistony@outlook.com>
2018-06-03 18:42:23 +0100

    Adds movie card message classes

---
+++ MoodMovies/Messages/MovieCardMessages.cs
@@ -0,0 +1,49 @@
+﻿using MoodMovies.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMovies.Messages
+{
+    public class AddToWatchListMessage
+    {
+        public AddToWatchListMessage(MovieCardViewModel mvCard)
+        {
+            MovieVM = mvCard;
+        }
+
+        public MovieCardViewModel MovieVM { get; private set; }
+    }
+
+    public class RemoveFromWatchListMessage
+    {
+        public RemoveFromWatchListMessage(MovieCardViewModel mvCard)
+        {
+            MovieVM = mvCard;
+        }
+
+        public MovieCardViewModel MovieVM { get; private set; }
+    }
+
+    public class AddToFavouritesMessage
+    {
+        public AddToFavouritesMessage(MovieCardViewModel mvCard)
+        {
+            MovieVM = mvCard;
+        }
+
+        public MovieCardViewModel MovieVM { get; private set; }
+    }
+
+    public class RemoveFromFavouritesMessage
+    {
+        public RemoveFromFavouritesMessage(MovieCardViewModel mvCard)
+        {
+            MovieVM = mvCard;
+        }
+
+        public MovieCardViewModel MovieVM { get; private set; }
+    }
+}

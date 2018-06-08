5cc0e6a8ad1de4ab203b6d9571357c200de18d86
Tony Karalis <karalistony@outlook.com>
2018-06-08 20:44:04 +0100

    Adds method to load moviecards from call to db

--- MoodMovies/ViewModels/FavouritesViewModel.cs
+++ MoodMovies/ViewModels/FavouritesViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using MoodMovies.Logic;
+using MoodMovies.Resources;
 
 namespace MoodMovies.ViewModels
 {
@@ -19,11 +21,42 @@ namespace MoodMovies.ViewModels
         #endregion
 
         #region Properties
+        readonly OfflineServiceProvider offDb = new OfflineServiceProvider();
+        #endregion
+
+        #region Properties
 
         #endregion
 
         #region Methods
+        public async Task LoadFavouriteItems()
+        {
+            try
+            {
+                var user = await offDb.GetFirstUser();      //replace by getting from static class**********
+                var movies = await offDb.GetAllWatchListItems(user);
+                //build up the movie card view models
+                Movies.Clear();
+                await Task.Run(() =>
+                {
+                    foreach (var movie in movies)
+                    {
+                        if (!string.IsNullOrEmpty(movie.Poster_path))
+                        {
+                        //force updating the list from a different thread using custom cross thread extension method
+                        Movies.AddOnUIThread(new MovieCardViewModel(movie.Movie_Id, movie.Title, new Uri(movie.Poster_path), movie.Overview,
+                            movie.Release_date, movie.Vote_count, movie.Vote_average, movie.Video, movie.Adult, movie.Popularity, movie.Original_language, eventAgg));
+                        }
+                    }
+                });
+            }
+            catch
+            {
+                throw;
+                //implement some error handling*************
+            }
 
+        }
         #endregion
 
     }

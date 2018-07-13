6468b60e18fa3abb1f4ea89b38fc64d4da99ec98
T.Karalis <t.karalis@mmr-research.com>
2018-07-13 12:30:01 +0100

    Adds login vm logic

--- MoodMovies/ViewModels/LoginViewModel.cs
+++ MoodMovies/ViewModels/LoginViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using Caliburn.Micro;
+using MaterialDesignThemes.Wpf;
+using MoodMovies.Logic;
+using MoodMovies.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,42 @@ using System.Threading.Tasks;
 
 namespace MoodMovies.ViewModels
 {
-    public class LoginViewModel : Screen
+    public class LoginViewModel : BaseViewModel
     {
+        public LoginViewModel(IEventAggregator _event, IOfflineServiceProvider offlineService, IOnlineServiceProvider onlineService, SnackbarMessageQueue statusMessage) : base(
+            _event, offlineService, onlineService, statusMessage)
+        {
+
+        }
+
+        #region  public Properties
+        private string _userEmail;
+        public string UserEmail { get => _userEmail; set { _userEmail = value; NotifyOfPropertyChange(); } }
+
+        private string _userPassword;
+        public string UserPassword { get => _userPassword; set { _userPassword = value; NotifyOfPropertyChange(); } }
+
+        private bool _keepLoggedIn;
+        public bool KeepLoggedIn { get => _keepLoggedIn; set { _keepLoggedIn = value; NotifyOfPropertyChange(); } }
+        #endregion
+
+        #region Public Methods
+        public void Login()
+        {
+            if (!string.IsNullOrEmpty(UserEmail)
+                && !string.IsNullOrEmpty(UserPassword))
+            {
+                eventAgg.PublishOnUIThread(new LoginMessage(UserEmail, UserPassword, KeepLoggedIn));
+            }
+            else
+            {
+                StatusMessage.Enqueue("Please complete all fields.");
+            }
+        }
+        #endregion
+
+        #region Private Methods
+       
+        #endregion
     }
 }

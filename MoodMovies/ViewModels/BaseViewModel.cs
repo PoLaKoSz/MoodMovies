cab13a601c1dd9ffce506be1e18b29f56ebd80fa
T.Karalis <t.karalis@mmr-research.com>
2018-07-13 09:30:02 +0100

    Adds baseviewmodel to hold common properties across new classes

---
+++ MoodMovies/ViewModels/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Caliburn.Micro;
+using MaterialDesignThemes.Wpf;
+using MoodMovies.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMovies.ViewModels
+{
+    public class BaseViewModel : Screen
+    {
+        public BaseViewModel(IEventAggregator _event, IOfflineServiceProvider offlineService, IOnlineServiceProvider onlineService, SnackbarMessageQueue statusMessage)
+        {
+            eventAgg = _event;
+            StatusMessage = statusMessage;
+            offlineDb = offlineService;
+            onlineDb = onlineService;
+        }
+
+        #region Events
+        public IEventAggregator eventAgg;
+        #endregion
+
+        #region Providers
+        protected IOfflineServiceProvider offlineDb;
+        protected IOnlineServiceProvider onlineDb;
+        #endregion
+
+        #region Properties
+        public SnackbarMessageQueue StatusMessage { get; set; }
+        #endregion
+    }
+}

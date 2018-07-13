fac1768e3eb891d4006da664d6aefe6a389ed115
T.Karalis <t.karalis@mmr-research.com>
2018-07-13 10:50:44 +0100

    Adds account message classes

---
+++ MoodMovies/Messages/AccountMessages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodMovies.Messages
+{
+    public class LoginMessage : IAccountMessage
+    {
+        public LoginMessage()
+        {
+
+        }
+    }
+
+    public class Registermessage :IAccountMessage
+    {
+        public  Registermessage()
+        {
+
+        }
+    }
+
+    public interface IAccountMessage
+    {
+
+    }
+}
--- MoodMovies/ViewModels/StartPageViewModel.cs
+++ MoodMovies/ViewModels/StartPageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using MaterialDesignThemes.Wpf;
+using MoodMovies.Logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,54 @@ using System.Threading.Tasks;
 
 namespace MoodMovies.ViewModels
 {
-    public class StartPageViewModel : Screen
+    public class StartPageViewModel : Conductor<Screen>.Collection.OneActive
     {
+        public StartPageViewModel(IEventAggregator _event, IOfflineServiceProvider offlineService, IOnlineServiceProvider onlineService, SnackbarMessageQueue statusMessage)
+        {
+            eventAgg = _event;
+            eventAgg.Subscribe(this);
+
+            offlineDb = offlineService;
+            onlineDb = onlineService;
+            StatusMessage = statusMessage; 
+        }
+
+        #region Events
+        public IEventAggregator eventAgg;
+        #endregion
+
+        #region Providers
+        readonly IOfflineServiceProvider offlineDb;
+        private IOnlineServiceProvider onlineDb;
+        #endregion
+
+        #region General Properties
+        private string _loadingMessage;
+        public string LoadingMessage { get => _loadingMessage; set { _loadingMessage = value; NotifyOfPropertyChange(); } }
+        private bool _isLoading;
+        public bool IsLoading { get => _isLoading; set { _isLoading = value; NotifyOfPropertyChange(); } }
+        public SnackbarMessageQueue StatusMessage { get; set; }
+        #endregion
+
+        #region Child View Models
+        private LoginViewModel _loginVM;
+        public LoginViewModel LoginVM { get => _loginVM; set { _loginVM = value; NotifyOfPropertyChange(); } }
+        private RegisterViewModel _registerVM;
+        public RegisterViewModel RegisterVM { get => _registerVM; set { _registerVM = value; NotifyOfPropertyChange(); } }
+        #endregion
+
+        #region Public Methods
+        public void ShowLoginPage()
+        {
+            DeactivateItem(ActiveItem, false);
+            ActivateItem(LoginVM);
+        }
+
+        public void ShowRegistrationPage()
+        {
+            DeactivateItem(ActiveItem, false);
+            ActivateItem(RegisterVM);
+        }
+        #endregion
     }
 }

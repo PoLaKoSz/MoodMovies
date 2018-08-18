0c635b524be8bb701824b20ce46a68188897aa71
Tony Karalis <karalistony@outlook.com>
2018-08-18 16:50:23 +0100

    Adds searchservice interface and class

---
+++ MoodMovies/Services/ISearchService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMdbEasy.TmdbObjects.Movies;
+
+namespace MoodMovies.Services
+{
+    public interface ISearchService
+    {
+        Task<MovieList> Search(string apiKey);
+    }
+}
---
+++ MoodMovies/Services/SearchService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMdbEasy.TmdbObjects.Movies;
+
+namespace MoodMovies.Services
+{
+    public class SearchService : ISearchService
+    {
+        public SearchService()
+        {
+
+        }
+
+        public async Task<MovieList> Search(string apiKey)
+        {
+            await Task.Delay(10);
+            return new MovieList();
+        }
+    }
+}

6989fe5df97fd947d81c7f93d70a2b0ba11743f8
PoLáKoSz <polakosz@freemail.hu>
2018-08-19 14:48:33 +0200

    Removed LoginMessage and RegisterMessage

--- MoodMovies/Messages/AccountMessages.cs
+++ MoodMovies/Messages/AccountMessages.cs
@@ -7,43 +7,6 @@ using System.Threading.Tasks;
 
 namespace MoodMovies.Messages
 {
-    public class LoginMessage : IAccountMessage
-    {
-        public LoginMessage(string email, string password, bool keeploggedin)
-        {
-            Email = email;
-            Password = password;
-            KeepLoggedIn = keeploggedin;
-        }
-
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool KeepLoggedIn { get; set; }
-    }
-
-    public class RegisterMessage :IAccountMessage
-    {
-        public  RegisterMessage(string firstname, string surname, string apikey, string email, string password)
-        {
-            FirstName = firstname;
-            Surname = surname;
-            ApiKey = apikey;
-            Email = email;
-            Password = password;
-        }
-
-        public string FirstName { get; set; }
-        public string Surname { get; set; }
-        public string ApiKey { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }      
-    }
-
-    public interface IAccountMessage
-    {
-
-    }
-
     public class LoggedInMessage
     {
         public LoggedInMessage(User user)
